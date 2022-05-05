8bbb494b487ba7e1fedcb825cdbb9f26af26935a
ChristianKastberg <56236299+ChristianKastberg@users.noreply.github.com>
2022-05-05 12:03:03 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from 'firebase/app'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD20nHADiE9oI6dAzQqAoOojTApE9SExB0",
+    authDomain: "hovedopgave-b742d.firebaseapp.com",
+    databaseURL: "https://hovedopgave-b742d-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "hovedopgave-b742d",
+    storageBucket: "hovedopgave-b742d.appspot.com",
+    messagingSenderId: "657233900674",
+    appId: "1:657233900674:web:1b6f97effc7d4bd693709b",
+    measurementId: "G-1TVJ7P5ZJW"
+  };
+
+
+  initializeApp(firebaseConfig)
